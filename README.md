862fe0e845ff5c8dc27d9bd2cad72d03218c390b
kimchild <allcreation@nate.com>
2021-05-05 21:49:38 +0900

    feat(test.js): 시간할당 해당시간에 맞는 객체 가져오기 추가
    번호가져오기, 시간 객체할당 추가

---
+++ README.md
@@ -0,0 +1,8 @@
+##Start
+* npm install
+* mkdir jest
+* npm init -y
+* npm i -D jest
+* package.json file -> "scripts": { "test": "jest" },
+* test.js -> test("1 is 1", () => { expecrt(1).toBe(1); });
+* npm test
--- test.js
+++ test.js
@@ -35,6 +35,10 @@ class Time {
       }
    }
 
+   getTime() {
+      this.today.getTime();
+   }
+
    getParseDate() {
       return this.yyyy + '-' + this.mm + '-' + this.dd + ' ' + this.hour + ':' + this.minutes;
    }
@@ -52,10 +56,12 @@ class Time {
 class EventDay {
    startTime;
    endTime;
+   number;
 
-   constructor(startTime, endTime) {
+   constructor(startTime, endTime, number) {
       this.startTime = new Time(startTime);
       this.endTime = new Time(endTime);
+      this.number = number;
       return this;
    }
 
@@ -63,31 +69,79 @@ class EventDay {
 
 class TimeDeal {
    eventDayList = [];
+   event;
    constructor() {
    }
 
-   addDay(startTime, endTime) {
-      this.eventDayList.push(new EventDay(startTime, endTime));
+   addDay(startTime, endTime, number) {
+      this.eventDayList.push(new EventDay(startTime, endTime, number));
    }
    getDay(index) {
       return this.eventDayList[index];
    }
+
+   isEvent(date) {
+      this.findEvent(date);
+      return (typeof this.event !== 'undefined');
+   }
+
+   findEvent(date) {
+      this.event = undefined;
+      for (let i = 0; i < this.eventDayList.length; i++) {
+         this.findTime(this.eventDayList[i], date);
+      }
+      return this.event;
+   }
+
+   isTime(value, date) {
+      return date.getTime() >= value.startTime.today.getTime()
+          && date.getTime() <= value.endTime.today.getTime();
+   }
+   findTime(value, date) {
+      if(this.isTime(value, date)) {
+         this.event = value;
+      }
+   }
 }
 
-// module.exports = MyClass;
+
+
+test('이벤트중인 이벤트번호 찾기', () => {
+   var timeDeal = new TimeDeal();
+   var number = 1111;
+   var number2 = 2222;
+   timeDeal.addDay(new Date('2021-05-05 10:00'), new Date('2021-05-05 23:59'), number);
+   timeDeal.addDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'), number2);
+
+   var event = {};
+   if(timeDeal.isEvent(new Date())) {
+      event = timeDeal.findEvent(new Date());
+   }
+   expect(event.number).toBe(number);
+});
+test('이벤트중 유무 찾기', () => {
+   var timeDeal = new TimeDeal();
+   var number = 1111;
+   var number2 = 2222;
+   timeDeal.addDay(new Date('2021-05-05 10:00'), new Date('2021-05-05 23:59'), number);
+   timeDeal.addDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'), number2);
+   expect(timeDeal.isEvent(new Date())).toBe(true);
+});
+test('이벤트 번호할당', () => {
+   var timeDeal = new TimeDeal();
+   var number = 1111;
+   timeDeal.addDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'), number);
+   expect(timeDeal.getDay(0).number).toBe(number);
+});
 test('이벤트날짜 목록 시간 등록 테스트', () => {
    var timeDeal = new TimeDeal();
    timeDeal.addDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'));
-   // timeDeal.addDay(new Date('2021-05-11 00:00'));
-   // timeDeal.addDay(new Date('2021-05-11 00:00'));
-   console.log('# timeDeal : ', timeDeal.getDay(0));
    expect(timeDeal.getDay(0).startTime.hour).toBe(10);
    expect(timeDeal.getDay(0).endTime.hour).toBe(14);
 });
 test('이벤트날짜 조회 테스트', () => {
    var timeDeal = new TimeDeal();
    timeDeal.addDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'));
-   console.log('# timeDeal : ', timeDeal.getDay(0));
    expect(timeDeal.getDay(0).startTime.dd).toBe(11);
 });
 test('이벤트날짜 생성 테스트', () => {
@@ -96,7 +150,6 @@ test('이벤트날짜 생성 테스트', () => {
 });
 test('이벤트생성 테스트', () => {
    var eventDay = new EventDay(new Date('2021-05-11 10:00'), new Date('2021-05-11 14:00'));
-   console.log('# eventDay : ', eventDay);
    expect(eventDay.startTime.dd).toBe(11);
 });
 test("1 is 1", () => {
